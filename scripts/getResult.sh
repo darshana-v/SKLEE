8a5c260a40333861eb8808bbcf642046262820ab
Pratyush Saini <66582958+pratyushiitd@users.noreply.github.com>
2021-06-21 20:15:57 +0530

    script documented

---
+++ scripts/getResult.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -u
+# commands to run KLEE and validate
+homeDir="{PWD}"
+toolPath="$homeDir/klee/tools/klee"
+kleeLibPath="$homeDir/klee/lib/Core"
+kleeIncludePath="$homeDir/klee/include/"
+cppLibPath="$homeDir/libc++-install-10/include/c++/v1/"
+
+python3 $toolPath/run.py 2 $toolPath
+
+if [ $? -eq 0 ]; then
+    echo ""
+else
+    echo "Error while fetching results."
+    printf "\n"
+    echo "Process terminated."
+    exit 1
+fi
+
+#chmod -R 777 $toolPath
+
+#sleep 1
+echo "Result:"
+echo "$(<$toolPath/result_new.txt)"
+if [ $? -eq 0 ]; then
+    echo ""
+else
+    echo "Error in fetching results."
+    exit 1
+fi
+
+echo "TestCases:"
+printf "\n"
+
+echo "$(<$toolPath/alltestcases.txt)"
+if [ $? -eq 0 ]; then
+    echo ""
+else
+    echo "Error in fetching testcases."
+    exit 1
+fi
+
+rm -f test* alltest* result* *.bc *.ll *.sol contract.txt names.txt logFile.txt properties.txt
+rm -f error*
+#awk '/^KLEE:/ {print}' result.txt
+
+
---
+++ scripts/runKLEE.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+#set -e
+set -u
+input="Voting.sol" #hardcoded
+
+#input=$1 #use with compile.php
+#convertedInput=${input::-4}
+convertedInput="convertedFile"
+cppInput="$convertedInput.cpp"
+bcInput="$convertedInput.bc"
+llInput="$convertedInput.ll"
+
+
+homeDir="./"
+toolPath="../klee/tools/klee"
+kleeLibPath="../klee/lib/Core"
+kleeIncludePath="../klee/include/"
+cppLibPath="../include/libc++-install-10/include/c++/v1/"
+outpath = "../output/"
+
+# Lexer and Grammar compilation
+
+flex -o $toolPath/lexer.c -l $toolPath/lexer.l &>logFile.txt
+echo "Lexer compilation finished."
+bison -vd -o $toolPath/grammar.c $toolPath/grammar.y >> logFile.txt
+echo "Grammar compilation finished."
+
+# Conversion of Solidity to C++.
+
+g++ -w -Wno-write-strings $kleeLibPath/defs.cpp $toolPath/lexer.c $toolPath/grammar.c -o $toolPath/verify -ll -std=c++11 #-ll &> logFile.txt
+echo "Executable genration finished"
+
+# If this does doesn't work, uncomment this 
+# chmod +x $toolPath/verify
+
+$toolPath/verify < $toolPath/$input >> logFile.txt
+
+printf "\n"
+if [ $? -eq 0 ]; then
+    echo "Conversion to intermediate code complete."
+else
+    echo "Solidity syntax/version error: could not convert contract file."
+    printf "\n"
+    echo "Process terminated."
+    exit 1
+fi
+
+echo "LLVM BitCode Generation"
+# clang supposed to be in the path, if not then add in the corresponding path
+#/usr/local/opt/llvm/bin/clang++ -O0 -Wno-everything -std=c++11 -I $cppLibPath -nostdinc++ -fsanitize=signed-integer-overflow -fsanitize=unsigned-integer-overflow -I $kleeIncludePath -emit-llvm -c -g $toolPath/$cppInput >> logFile.txt
+clang++ -O0 -Wno-everything -std=c++11 -I $cppLibPath -nostdinc++ -fsanitize=signed-integer-overflow -fsanitize=unsigned-integer-overflow -I $kleeIncludePath -emit-llvm -c -g $outpath/$cppInput >> logFile.txt
+
+if [ $? -eq 0 ]; then
+    echo "Conversion to LLVM Bitcode complete."
+else
+    echo "Error in bitcode file, needs refinement."
+    printf "\n"
+    echo "Process terminated."
+    exit 1
+fi
+
+../include/llvm-10/bin/llvm-dis $outpath/$bcInput
+
+#/usr/local/opt/llvm/bin/llvm-dis $bcInput
+
+chmod -R 777 $toolPath
+
+chmod -R 777 ../build/bin/klee
+
+# TODO: Klee needs to be rebuild: URGENT
+
+../build/bin/klee --libc=uclibc -libcxx -posix-runtime --max-instructions=2000000 --warnings-only-to-file $outpath/$llInput
+
+
+if [ $? -eq 0 ]; then
+    echo "KLEE run complete.\nKLEE Test cases generated"
+else
+    echo "Error while running KLEE: timeout or SIGSEV."
+    printf "\n"
+    echo "Process terminated."
+    exit 1
+fi
+
+
+#chmod -R 777 $toolPath
+
+#printf "\n"
+
+#echo "Properties violated in result.txt"
+#echo "Validating on private Ethereum Blockchain..."
+
+#printf "\n"
+#if [ $1 == "BPUseNet.sol" ]
+#then
+#	$homeDir/build/bin/ktest-tool $toolPath/klee-last/error-MultOverflow000001.ktest &> error-MultOverflow.txt
+#	$homeDir/build/bin/ktest-tool $toolPath/klee-last/test000002.ktest &> testcase1.txt
+#	$homeDir/build/bin/ktest-tool $toolPath/klee-last/test000003.ktest &> testcase2.txt
+#else
+#	$homeDir/build/bin/ktest-tool $toolPath/klee-last/test000001.ktest &> testcase1.txt
+#	$homeDir/build/bin/ktest-tool $toolPath/klee-last/test000002.ktest &> testcase2.txt
+#fi	
+
+#../build/bin/ktest-tool klee-last/test000002.ktest &> testcase2.txt
+
+
+python3 $toolPath/run.py 1 $toolPath
+
+if [ $? -eq 0 ]; then
+    echo ""
+else
+    echo "Error while generating testcases."
+    printf "\n"
+    echo "Process terminated."
+    exit 1
+fi
+
+
