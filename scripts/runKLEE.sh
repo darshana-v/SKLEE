1b299ffab01a7e818a521ab6d72c7d68a3065e6d
Namrata Jain <34477711+namrata690@users.noreply.github.com>
2022-08-07 12:25:11 +0530

    Update runKLEE.sh

--- scripts/runKLEE.sh
+++ scripts/runKLEE.sh
@@ -1,42 +1,29 @@
 #!/bin/bash
 #set -e
 set -u
-input="Voting.sol" #hardcoded
-
-#input=$1 #use with compile.php
+input=$1
 #convertedInput=${input::-4}
 convertedInput="convertedFile"
 cppInput="$convertedInput.cpp"
 bcInput="$convertedInput.bc"
 llInput="$convertedInput.ll"
+# commands to run KLEE and validate
+homeDir="./"
+toolPath="./klee/tools/klee"
+kleeLibPath="./klee/lib/Core"
+kleeIncludePath="./klee/include/"
+cppLibPath="./libc++-install-10/include/c++/v1/"
+#cd ~/Desktop/project/verifTool/
+echo "Converting Contract code to LLVM bitcode..."
 
+flex -o $toolPath/lexer.c -l $toolPath/lexer.l >>logFile.txt
+bison -vd -o $toolPath/grammar.c $toolPath/grammar2Copy.y >>logFile.txt
 
-homeDir="../"
-toolPath="../klee/tools/klee"
-kleeLibPath="../klee/lib/Core"
-kleeIncludePath="../klee/include/"
-cppLibPath="../include/libc++-install-10/include/c++/v1/"
-outpath="../output"
-cpp_path="../output/convertedFile.cpp"
+#echo $input
+#echo $convertedInput
+g++ -w -Wno-write-strings $kleeLibPath/defs.cpp $toolPath/lexer.c $toolPath/grammar.c -o $toolPath/verify -ll >>logFile.txt 
 
-# Lexer and Grammar compilation
-
-flex -o $toolPath/lexer.c -l $toolPath/lexer.l # &>logFile.txt
-echo "Lexer compilation finished."
-bison -vd -o $toolPath/grammar.c $toolPath/grammar.y #>> logFile.txt
-echo "Grammar compilation finished."
-
-# Conversion of Solidity to C++.
-
-g++ -w -Wno-write-strings $kleeLibPath/defs.cpp $toolPath/lexer.c $toolPath/grammar.c -o $toolPath/verify -ll -std=c++11 #-ll &> logFile.txt
-echo "Executable generation finished"
-
-# If this does doesn't work, uncomment this 
-# chmod +x $toolPath/verify
-
-$toolPath/verify < $outpath/$input  #>> logFile.txt
-
-printf "\n"
+$toolPath/verify < $toolPath/$input >>logFile.txt
 if [ $? -eq 0 ]; then
     echo "Conversion to intermediate code complete."
 else
@@ -46,11 +33,8 @@ else
     exit 1
 fi
 
-echo "LLVM BitCode Generation"
 
-# clang supposed to be in the path, if not then add in the corresponding path
-#/usr/local/opt/llvm/bin/clang++ -O0 -Wno-everything -std=c++11 -I $cppLibPath -nostdinc++ -fsanitize=signed-integer-overflow -fsanitize=unsigned-integer-overflow -I $kleeIncludePath -emit-llvm -c -g $toolPath/$cppInput >> logFile.txt
-clang++ -O0 -Wno-everything -std=c++11 -I $cppLibPath -nostdinc++ -fsanitize=signed-integer-overflow -fsanitize=unsigned-integer-overflow -I $kleeIncludePath -emit-llvm -c -g $cpp_path >> logFile.txt
+clang++ -O0 -Wno-everything -std=c++11 -I $cppLibPath -nostdinc++ -fsanitize=signed-integer-overflow -fsanitize=unsigned-integer-overflow -I $kleeIncludePath -emit-llvm -c -g $toolPath/$cppInput &> logFile.txt
 
 if [ $? -eq 0 ]; then
     echo "Conversion to LLVM Bitcode complete."
@@ -60,26 +44,16 @@ else
     echo "Process terminated."
     exit 1
 fi
-mv convertedFile.bc ../output/
 
-chmod +x ../include/llvm-10/bin/llvm-dis
-
-# ../include/llvm-10/bin/llvm-dis $outpath/$bcInput -o $outpath/$llInput
-
-llvm-dis $outpath/$bcInput -o $outpath/$llInput
+llvm-dis $bcInput
 
 #chmod -R 777 $toolPath
 
-chmod +x ../build/bin/klee
-
-# TODO: Klee needs to be rebuild: URGENT
-
-#/usr/local/opt/klee/bin/klee --libc=uclibc -libcxx -posix-runtime --max-instructions=2000000 --warnings-only-to-file $outpath/$llInput
-../build/bin/klee --libc=uclibc -libcxx -posix-runtime --max-instructions=2000000 --warnings-only-to-file $outpath/$llInput
+./build/bin/klee --libc=uclibc -libcxx -posix-runtime --max-instructions=2000000 --warnings-only-to-file $llInput
 
 
 if [ $? -eq 0 ]; then
-    echo "KLEE run complete.\nKLEE Test cases generated"
+    echo "KLEE run complete."
 else
     echo "Error while running KLEE: timeout or SIGSEV."
     printf "\n"
@@ -87,27 +61,12 @@ else
     exit 1
 fi
 
-
 #chmod -R 777 $toolPath
 
 #printf "\n"
 
 #echo "Properties violated in result.txt"
 #echo "Validating on private Ethereum Blockchain..."
-
-#printf "\n"
-#if [ $1 == "BPUseNet.sol" ]
-#then
-#	$homeDir/build/bin/ktest-tool $toolPath/klee-last/error-MultOverflow000001.ktest &> error-MultOverflow.txt
-#	$homeDir/build/bin/ktest-tool $toolPath/klee-last/test000002.ktest &> testcase1.txt
-#	$homeDir/build/bin/ktest-tool $toolPath/klee-last/test000003.ktest &> testcase2.txt
-#else
-#	$homeDir/build/bin/ktest-tool $toolPath/klee-last/test000001.ktest &> testcase1.txt
-#	$homeDir/build/bin/ktest-tool $toolPath/klee-last/test000002.ktest &> testcase2.txt
-#fi	
-
-#../build/bin/ktest-tool klee-last/test000002.ktest &> testcase2.txt
-
 
 python3 $toolPath/run.py 1 $toolPath
 
@@ -119,5 +78,3 @@ else
     echo "Process terminated."
     exit 1
 fi
-
-
